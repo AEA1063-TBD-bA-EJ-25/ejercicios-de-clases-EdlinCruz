0c194a0c58e42d521308bdac5d83bfed4f5fdfae
EdlinCruz <L23170042@culiacan.tecnm.mx>
2025-02-26 17:57:35 +0000

    creacion final

--- Scripts/2025/crearTablas.sql
+++ Scripts/2025/crearTablas.sql
@@ -22,3 +22,33 @@ CREATE TABLE Alumno (
     ClaveCarrera CHAR(13) FOREIGN KEY REFERENCES Carrera(Clave)
 )
 select * from persona;
+
+CREATE TABLE Docente (
+    CURP CHAR (18) NOT NULL PRIMARY KEY,
+    RFC CHAR(13) NOT NULL UNIQUE,
+    Profesion NVARCHAR(30) NULL,
+    FOREIGN KEY (CURP) REFERENCES Persona(CURP)
+)
+
+    CREATE TABLE Administrativo(
+        CURP CHAR (18) NOT NULL
+        PRIMARY KEY 
+        FOREIGN KEY REFERENCES Persona(CURP)
+    )
+
+INSERT INTO Carrera (Clave, Nombre )
+VALUES ('ITIC-2010-225', 'TICs')
+
+select * from Carrera
+INSERT INTO Carrera (Clave, Nombre)
+VALUES('ISIC-2010-224', 'Ingenieria en Sistemas Computacionales'),
+    ('IMTC-2010-224','Ingenieria en mecatranica' )
+
+    INSERT INTO Persona (CURP, Nombre, Direccion, Nacimiento)
+        VALUES('JS20FR', 'Jphane Sacrebleu', 'Por alla', '2033-04-05')
+
+    INSERT INTO Alumnno(CURP, NumeroDeControl, CarreraClave)
+        VALUES('JS20FR', '20170005', 'ITIC-2010-225')
+
+        select NumeroDeControl, Nombre, CarreraClave from Persona
+        join Alumno on Persona.CURP = Alumno.CURP
