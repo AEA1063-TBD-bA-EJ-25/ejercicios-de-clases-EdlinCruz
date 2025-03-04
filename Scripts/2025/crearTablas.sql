e58e3dfd80e6910405c97ea82aff31c7d99ef22d
EdlinCruz <L23170042@culiacan.tecnm.mx>
2025-03-04 17:56:53 +0000

    04Marzo

--- Scripts/2025/crearTablas.sql
+++ Scripts/2025/crearTablas.sql
@@ -5,34 +5,34 @@ create database escuelita;
 go
 
 use escuelita
-create TABLE Personaa(
+create TABLE Persona(
     CURP CHAR(18) NOT NULL PRIMARY KEY,
     Nombre NVARCHAR(50) NOT NULL,
     Direccion NVARCHAR (100),
     Nacimiento DATE NOT NULL,
 )
 
-CREATE TABLE Carreraa(
+CREATE TABLE Carrera(
     Clave CHAR(13) NOT NULL PRIMARY KEY,
     Nombre VARCHAR(60) NOT NULL
 
 )
 
-CREATE TABLE Alumnoo (
+CREATE TABLE Alumno(
     CURP CHAR(18) NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES Personaa(CURP),
     NumeroDeControl CHAR (8) NOT NULL UNIQUE,
     ClaveCarrera CHAR(13) FOREIGN KEY REFERENCES Carreraa(Clave)
 )
 select * from Personaa;
 
-CREATE TABLE Docentee (
+CREATE TABLE Docente(
     CURP CHAR (18) NOT NULL PRIMARY KEY,
     RFC CHAR(13) NOT NULL UNIQUE,
     Profesion NVARCHAR(30) NULL,
     FOREIGN KEY (CURP) REFERENCES Personaa(CURP)
 )
 
-    CREATE TABLE Administrativoo(
+    CREATE TABLE Administrativo(
         CURP CHAR (18) NOT NULL
         PRIMARY KEY 
         FOREIGN KEY REFERENCES Personaa(CURP)
@@ -56,3 +56,53 @@ select * from Carreraa
     /*select * from Alumno*/
         select NumeroDeControl, Nombre, ClaveCarrera from Personaa
         join Alumno on Personaa.CURP = Alumno.CURP
+
+
+        create table empleado(
+            idEmpleado int not null primary key,
+            nombre NVARCHAR(50) not null,
+            sueldo money DEFAULT 1500
+        )
+            
+        
+
+        INSERT INTO empleado(idEmpleado, nombre, sueldo)
+        values(1, 'Felipe de jesus', 2000)
+
+        insert into empleado(idEmpleado, nombre)
+        values(2, 'Chuerk')
+
+        SELECT * from empleado
+
+        CREATE TABLE personita(
+            idEmpleado int not null primary key,
+            nombre NVARCHAR(50) not null,
+            sueldo money DEFAULT 1500,
+            telefono VARCHAR(20) check (telefono like '667[0-9][0-9][0-9]')
+        )
+
+        insert into personita values (1, 'Juana', null, '667123')
+
+        ALTER table empleado
+            add sexo char(1) null check(sexo in('H', 'M'))
+
+        select * from empleado
+        insert into empleado(idEmpleado, nombre, sexo) values (3, 'Alexis Jara Rodriguez', 'H')
+         insert into empleado(idEmpleado, nombre, sexo) values (4, 'Matilde Sandoval', 'H')
+
+         alter table empleado 
+         drop CONSTRAINT CK_empleado_sexo_3F466844
+
+         INSERT INTO empleado(idEmpleado, nombre, sexo) values(4, 'Matilde Sandoval', 'H')
+         select * from empleado
+
+         alter table empleado
+            ADD CONSTRAINT sexonuevasopciones check (sexo in ('H', 'M','T','F'))
+
+        insert into empleado (idEmpleado, nombre, sexo) values (5, 'refugio Saldivar', 'F')
+        insert into empleado(idEmpleado, nombre, sexo) values (6, 'Delia Barrantes', 'R')
+
+
+
+        alter table empleado
+            add nacimiento date not null check(date <getDate())
